f4a27dc07f77fa11fa65c3d50767472425ebdaa9
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-01-18 18:01:27 -0300

    LineBy_Python

---
+++ chapter_01/chapter_init.py
@@ -0,0 +1,36 @@
+# - Why Phyton?
+Head = '''
+    LineByLine Apresenta
+    LineBy_Python
+    Created by - Hernaldo Meneses    
+    Welcome to Chapter 01
+
+'''
+why_python = '''    
+    Eficiencia
+    Código-Limpo
+    Multiplos-propósitos
+    Comunidade-Mundial
+    Assecibilidade
+    Facilidade
+
+'''
+
+base = '''    
+    1 - Intalação Python - Hello world!
+    2 - Variáveis - (textos - numeros)
+    3 - Listas
+    4 - Intrução - If
+    5 - Dicionários
+    6 - Entrada do Usuário
+    7 - Laços - While
+    8 - Funções
+    9 - Classes
+    10 - Arquivos
+    11 - Excessões
+    12 - Testes
+
+'''
+print(Head)
+print(why_python)
+print(base)
