62a70c75a73d4216bc41c49ee0edfe3a2dc9f7dc
HernaldoMeneses <hernaldomeneses@gmail.com>
2024-03-03 16:14:32 -0300

    01_Work.py

---
+++ 04_Trabalhando_Listas/01_Work.py
@@ -0,0 +1,25 @@
+#Pecorrendo todos itens de uma lista com laço for
+#For iten in list:
+#Identeition matters
+
+magicians = [
+    'alice',
+    'david',
+    'carolina'
+    ]
+
+print(magicians)
+
+for i_list in magicians:
+    print(i_list)
+
+print("\n")
+for i_list in magicians:
+    print("\t" + i_list.title())
+
+print("\n")
+for i_list in magicians:
+    print("\t\tHello, " + i_list.title() + "!")
+    print("\t\t\tI can't wait to see your next trick," + i_list.title() +"!")
+
+#continue.. 90/592
---
+++ 04_Trabalhando_Listas/ex.cmd
@@ -0,0 +1,2 @@
+call python 01_Work.py
+cmd
